9752ed0650d54990229b8e7d5489a030518fdd7e
Matthew Miller <matt.miller@nfiindustries.com>
2021-07-30 14:13:44 -0500

    Prepare for Hex release

--- mix.exs
+++ mix.exs
@@ -4,17 +4,20 @@ defmodule Terrasol.MixProject do
   def project do
     [
       app: :terrasol,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.12",
+      name: "Terrasol",
+      source_url: "https://github.com/mwmiller/terrasol",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   defp deps do
@@ -23,6 +26,24 @@ defmodule Terrasol.MixProject do
       {:equivalex, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:ex_doc, "~>  0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Terrasol - a pure Elixir library for handling Earthstar data
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Matt Miller"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/mwmiller/terrasol",
+        "Earthstar" => "https://earthstar-docs.netlify.app/"
+      }
     ]
   end
 end
